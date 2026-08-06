02b434549efe2d056e9af2f1abfea86e8f1c898a
rambuvn <vn.nqhung@gmail.com>
2026-08-06 10:17:28 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,16 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import { GoogleTagManager } from '@/components/google-tag-manager'
 import './globals.css'
 
-const geist = Geist({ subsets: ['latin'], variable: '--font-geist' })
-const geistMono = Geist_Mono({ subsets: ['latin'], variable: '--font-geist-mono' })
+const geist = Geist({
+  subsets: ['latin'],
+  variable: '--font-geist',
+  display: 'swap',
+})
+const geistMono = Geist_Mono({
+  subsets: ['latin'],
+  variable: '--font-geist-mono',
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
 title: 'rambudev | Senior WordPress Development',
